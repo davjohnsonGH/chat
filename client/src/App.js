fc8c8125bcd52acf8490e7206b58dcb7a704cca4
David Johnson <davidjohnson@Davids-MacBook-Pro.local>
2019-11-19 22:12:36 -0800

    Adds client side socket code to connect a user.

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { render } from "react-dom";
+import socketIOClient from "socket.io-client";
+
 
 const App = () => {
+
+  const endpoint = "http://localhost:3000/"
+  const socket = socketIOClient(endpoint);
+
+
+  useEffect( () => {
+
+    socket.on("FromAPI", ( data ) => {
+      console.log(data)
+    });      
+
+  }, [])
+
+
   return (
     <div>
       <h1>five minute react starter</h1>
