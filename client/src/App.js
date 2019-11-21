f69d019a08acb5fa62473cb21371e097890789d6
David Johnson <davidjohnson@Davids-MacBook-Pro.local>
2019-11-20 22:24:39 -0800

    Adds input fields for chat and handle submissions. Also inludes socket emit and a log of emit proof in index js server file.

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { render } from "react-dom";
 import socketIOClient from "socket.io-client";
 
@@ -7,6 +7,11 @@ const App = () => {
 
   const endpoint = "http://localhost:3000/"
   const socket = socketIOClient(endpoint);
+  const [ handle, updateHandle ] = useState("");
+  const [ displayHandle, updateDisplayHandle ] = useState("");
+  const [ chat, updateChat ] = useState("");
+  const [ displayChat, updateDisplayChat ] = useState("");
+
 
 
   useEffect( () => {
@@ -15,12 +20,47 @@ const App = () => {
       console.log(data)
     });      
 
-  }, [])
+  }, []);
 
+  function setDisplayHandle(e) {
+    e.preventDefault();
+    updateDisplayHandle(handle);
+  }
+  function setChat(e) {
+    e.preventDefault();
+    updateDisplayChat(chat)
+    socket.emit('chat message', chat);
+  }
 
   return (
-    <div>
-      <h1>five minute react starter</h1>
+    <div className="chat-wrapper">
+      { displayHandle.length > 0 ? "Hello " + displayHandle : "" }
+      { displayChat.length > 0 ? "Chat: " + displayChat: "" }
+      <form>
+
+        <label htmlFor="handle-input"> 
+          <input 
+            id="handle-input" 
+            type="text"
+            value={ handle } 
+            placeholder="Handle"
+            onChange={e => updateHandle(e.target.value)}> 
+          </input>
+        </label>
+        <input type="submit" value="Submit" onClick={ e => setDisplayHandle(e) }/>
+
+        <label htmlFor="chat-input"> 
+          <input 
+            id="chat-input" 
+            type="text"
+            value={ chat } 
+            placeholder="Chat"
+            onChange={e => updateChat(e.target.value)}> 
+          </input>
+        </label>
+        <input type="submit" value="Submit" onClick={ e => setChat(e) }/>  
+
+      </form>
     </div>
   );
 };
--- server/index.js
+++ server/index.js
@@ -8,6 +8,15 @@ app.get('/', function(req, res){
 
 io.on('connection', function(socket){
     console.log('a user connected');
+    // leverage the socket obj api 
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
+      });    
+
+      socket.on('chat message', function(msg){
+        console.log('message: ' + msg);
+      });      
+
 });
 
 http.listen(3000, function(){
